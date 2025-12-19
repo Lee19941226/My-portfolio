9ee2493bdacb54cf0f9c5939fbf6bd15bf291eff
yeah~ <skjb6996@naver.com>
2025-12-19 16:46:00 +0900

    Update portfolio

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -108,7 +108,7 @@ const Header = () => {
 
         <h1 className="header_logo_title">
           <Link to="#home" onClick={(e) => handleNavClick(e, "#home")}>
-            나는 짱이다
+            나는 짱이다1234
           </Link>
         </h1>
       </div>
